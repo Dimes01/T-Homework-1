b9b47d9568b2703e10eb986e0e77e5e4581b825c
Dimes01 <tyapin2002@mail.ru>
2024-09-09 22:20:29 +0400

    feat: написаны тесты
    Signed-off-by: Dimes01 <tyapin2002@mail.ru>

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,18 +1,3 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import models.lesson2.City;
-import org.example.Main;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.slf4j.Logger;
-
-import java.io.File;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 public class MainTest {
 
 }
