e61742dcc5da6ad2ea4c594f40e4b2a1aada5ffd
Dimes01 <tyapin2002@mail.ru>
2024-09-09 22:19:55 +0400

    fix: использование Lombok в ListNode
    Signed-off-by: Dimes01 <tyapin2002@mail.ru>

--- src/main/java/models/lesson3/ListNode.java
+++ src/main/java/models/lesson3/ListNode.java
@@ -1,39 +1,13 @@
 package models.lesson3;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class ListNode<T> {
     private T value;
     private ListNode<T> previousNode;
     private ListNode<T> nextNode;
-
-    public ListNode(T value, ListNode<T> previousNode, ListNode<T> nextNode) {
-        this.value = value;
-        this.previousNode = previousNode;
-        this.nextNode = nextNode;
-    }
-
-    public T getValue() {
-        return value;
-    }
-
-    public void setValue(T newValue) {
-        value = newValue;
-    }
-
-    public ListNode<T> getNext() {
-        return nextNode;
-    }
-
-    public ListNode<T> getPrevious() {
-        return previousNode;
-    }
-
-    public void setNext(ListNode<T> node) {
-        nextNode = node;
-    }
-
-    public void setPrevious(ListNode<T> node) {
-        previousNode = node;
-    }
 }
