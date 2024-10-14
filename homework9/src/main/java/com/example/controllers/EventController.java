8e676c7c7c6c1322cd698216636e9d0f7ab9c58b
Dimes01 <tyapin2002@mail.ru>
2024-10-14 11:12:34 +0400

    feat: написано третье задание, но не протестировано

--- homework9/src/main/java/com/example/controllers/EventController.java
+++ homework9/src/main/java/com/example/controllers/EventController.java
@@ -13,10 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @RestController
 public class EventController {
@@ -29,28 +33,30 @@ public class EventController {
     private KudaGOService kudaGOService;
 
     @GetMapping("/events")
-    public ResponseEntity<List<PossibleEvent>> getPossibleEvents(
+    public Mono<ResponseEntity<List<PossibleEvent>>> getPossibleEvents(
             @RequestParam double budget,
             @RequestParam String currency,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateFrom,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateTo
     ) {
-        CompletableFuture<List<Event>> eventsFuture = CompletableFuture.supplyAsync(() ->
-            kudaGOService.getPossibleEvents(dateFrom, dateTo)
-        );
+        Flux<Event> eventsFlux = kudaGOService.getPossibleEventsFlux(dateFrom, dateTo);
 
-        CompletableFuture<ConvertResponse> convertFuture = CompletableFuture.supplyAsync(() -> restClient.post()
+        Mono<ConvertResponse> convertMono = Mono.just(Objects.requireNonNull(restClient.post()
                 .uri("/convert")
                 .body(new ConvertRequest(currency, "RUB", budget))
                 .retrieve()
-                .body(ConvertResponse.class)
-        );
+                .body(ConvertResponse.class)));
 
-        List<PossibleEvent> result = eventsFuture.thenCombine(convertFuture, (events, convert) ->
-                kudaGOService.filterEventsByBudget(events, convert.getConvertedAmount()).stream()
-                        .map((event) -> new PossibleEvent(event.getName(), event.getDates(), event.getMinCost(), event.getMaxCost()))
-                        .toList()).resultNow();
-
-        return ResponseEntity.ok(result);
+        return Mono.zip(eventsFlux.collectList(), convertMono)
+                .flatMap(tuple -> {
+                    List<Event> events = tuple.getT1();
+                    ConvertResponse convert = tuple.getT2();
+                    double convertedAmount = convert.getConvertedAmount();
+                    return kudaGOService.filterEventsByBudget(Flux.fromIterable(events), convertedAmount).collectList();
+                })
+                .map(filteredEvents -> filteredEvents.stream()
+                        .map(event -> new PossibleEvent(event.getName(), event.getDates(), event.getMinCost(), event.getMaxCost()))
+                        .collect(Collectors.toList()))
+                .map(ResponseEntity::ok);
     }
 }
--- homework9/src/main/java/com/example/services/KudaGOService.java
+++ homework9/src/main/java/com/example/services/KudaGOService.java
@@ -5,12 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,16 +33,7 @@ public class KudaGOService {
         List<Event> result = new ArrayList<>();
         while (has_next) {
             int pageForRequest = page;
-            Event[] events = restClient.get()
-                    .uri(uriBuilder -> uriBuilder.path("/public-api/v1.4/events/")
-                            .queryParam("page", pageForRequest)
-                            .queryParam("page_size", page_size)
-                            .queryParam("fields", "id,price,dates")
-                            .queryParam("actual_since", from.getTime())
-                            .queryParam("actual_until", to.getTime())
-                            .build())
-                    .retrieve()
-                    .body(Event[].class);
+            Event[] events = getEventsPage(pageForRequest, page_size, from, to).body(Event[].class);
             result.addAll(events != null ? List.of(events) : List.of());
             has_next = !result.isEmpty();
             if (!has_next)
@@ -50,9 +45,36 @@ public class KudaGOService {
         return result;
     }
 
+    public Flux<Event> getPossibleEventsFlux(Date from, Date to) {
+        return Flux.defer(() -> Flux.range(1, Integer.MAX_VALUE)
+                .concatMap(page -> getEventsPageMono(getEventsPage(page, 100, from, to)))
+                .takeUntil(List::isEmpty)
+                .flatMap(Flux::fromIterable));
+    }
+
     public List<Event> filterEventsByBudget(List<Event> events, double budget) {
         return events.stream()
                 .filter(event -> event.getMinCost() <= budget)
                 .collect(Collectors.toList());
+    }
+
+    public Flux<Event> filterEventsByBudget(Flux<Event> events, double budget) {
+        return events.filter(event -> event.getMinCost() <= budget);
+    }
+
+    private Mono<List<Event>> getEventsPageMono(RestClient.ResponseSpec responseSpec) {
+        return Mono.just(List.of(Objects.requireNonNull(responseSpec.body(Event[].class))));
+    }
+
+    private RestClient.ResponseSpec getEventsPage(int page, int page_size, Date from, Date to) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/public-api/v1.4/events/")
+                        .queryParam("page", page)
+                        .queryParam("page_size", page_size)
+                        .queryParam("fields", "id,price,dates")
+                        .queryParam("actual_since", from.getTime())
+                        .queryParam("actual_until", to.getTime())
+                        .build())
+                .retrieve();
     }
 }
